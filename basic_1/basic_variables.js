428b85ce813f06bcfd51ea56a4ff2b74be2be02d
Prashant Sharma <73147009+Genius-78@users.noreply.github.com>
2023-12-01 18:05:39 +0000

    dataTypes

---
+++ basic_1/basic_variables.js
@@ -0,0 +1,8 @@
+const accountId = '12345'
+let accountEmail = 'ps@gmail.com'
+var accountPass = '12345678'
+accountCity = 'jaipur'
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPass, accountCity]);
